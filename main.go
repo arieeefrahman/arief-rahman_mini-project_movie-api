6edcb550b52b3206a6474d8ccfbd45f5ba382eb1
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-10-30 16:32:54 +0700

    initial commit app

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	_middlewares "mini-project-movie-api/app/middlewares"
+	_util "mini-project-movie-api/utils"
+	_routes "mini-project-movie-api/app/routes"
+
+	_driverFactory "mini-project-movie-api/drivers"
+	_dbDriver "mini-project-movie-api/drivers/mysql"
+
+	_genreUseCase "mini-project-movie-api/businesses/genres"
+	_movieUseCase "mini-project-movie-api/businesses/movies"
+	_ratingUseCase "mini-project-movie-api/businesses/ratings"
+	_userUseCase "mini-project-movie-api/businesses/users"
+
+	_genreController "mini-project-movie-api/controllers/genres"
+	_movieController "mini-project-movie-api/controllers/movies"
+	_ratingController "mini-project-movie-api/controllers/ratings"
+	_userController "mini-project-movie-api/controllers/users"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	configDB := _dbDriver.ConfigDB{
+		DB_USERNAME: _util.GetConfig("DB_USERNAME"),
+		DB_PASSWORD: _util.GetConfig("DB_PASSWORD"),
+		DB_HOST: _util.GetConfig("DB_HOST"),
+		DB_PORT: _util.GetConfig("DB_PORT"),
+		DB_NAME: _util.GetConfig("DB_NAME"),
+	}
+
+	db := configDB.InitDB()
+
+	_dbDriver.DBMigrate(db)
+
+	configJWT := _middlewares.ConfigJWT{
+		SecretJWT: _util.GetConfig("JWT_SECRET_KEY"),
+		ExpiresDuration: 1,
+	}
+
+	configLogger := _middlewares.ConfigLogger{
+		Format: "[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}" + "\n",
+	}
+
+	app := echo.New()
+
+	genreRepo := _driverFactory.NewGenreRepository(db)
+	genreUseCase := _genreUseCase.NewGenreUsecase(genreRepo)
+	genreCtrl := _genreController.NewGenreController(genreUseCase)
+
+	movieRepo := _driverFactory.NewMovieRepository(db)
+	movieUseCase := _movieUseCase.NewMovieUseCase(movieRepo)
+	movieCtrl := _movieController.NewMovieController(movieUseCase)
+
+	ratingRepo := _driverFactory.NewRatingRepository(db)
+	ratingUseCase := _ratingUseCase.NewRatingUseCase(ratingRepo)
+	ratingCtrl := _ratingController.NewRatingController(ratingUseCase)
+
+	userRepo := _driverFactory.NewUserRepository(db)
+	userUseCase := _userUseCase.NewUserUsecase(userRepo, &configJWT)
+	userCtrl := _userController.NewAuthController(userUseCase)
+
+	routesInit := _routes.ControllerList{
+		LoggerMiddleware: configLogger.Init(),
+		JWTMiddleware: configJWT.Init(),
+		GenreController: *genreCtrl,
+		MovieController: *movieCtrl,
+		RatingController: *ratingCtrl,
+		AuthController: *userCtrl,
+	}
+
+	routesInit.RouteRegister(app)
+
+	app.Logger.Fatal(app.Start(":8000"))
+}
